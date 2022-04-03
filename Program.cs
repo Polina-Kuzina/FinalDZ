7952198d0e34fcb50924be566060dba0e0c32d87
Polina <payline@mail.ru>
2022-04-03 18:48:27 +0300

    Вывод массива

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Финальное ДЗ
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+string[] text = { "hello", "2", "world", ":-)" };
+Console.WriteLine("Исходный массив:");
+PrintArray(text);
