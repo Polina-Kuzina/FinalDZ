f775bf4fb17703b1c3d9fa52934e60f034048b80
Polina <payline@mail.ru>
2022-04-03 19:04:15 +0300

    Длина нового массива

--- Program.cs
+++ Program.cs
@@ -10,3 +10,8 @@ void PrintArray(string[] array)
 string[] text = { "hello", "2", "world", ":-)" };
 Console.WriteLine("Исходный массив:");
 PrintArray(text);
+int sizeNewArray = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i].Length < 4) sizeNewArray++;
+}
